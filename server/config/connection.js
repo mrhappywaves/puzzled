4e129860c9ac302cb42100a3eb84aab751ed2950
MrCommit <maxirybakin@gmail.com>
2022-05-21 11:42:22 -0700

    Working server with mongo connected

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_ENDPOINT || '', {
+const mongoAddressDB = 'mongodb+srv://new_user_2:OAoKqXKoK2ZNMmzm@cluster0.zictj.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(process.env.MONGODB_ENDPOINT || mongoAddressDB, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
